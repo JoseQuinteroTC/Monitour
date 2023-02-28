4f6eb462b27c593cfe524dc75440ff3c4beebb0f
Salvador Sanabria <salvadorsanabria14@gmail.com>
2023-02-28 18:06:58 -0500

    ajustes navbar y footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('src/app/pages/home/home.module').then(
+            (m) => m.HomeModule
+          ),
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

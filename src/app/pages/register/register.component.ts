828a412fa5dd1dc176c5317b9708e464fb7f4e8d
Salvador Sanabria <salvadorsanabria14@gmail.com>
2023-03-31 21:10:05 -0500

    validaciones registro

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
 
+  constructor(
+    private fb: FormBuilder
+  ){ }
+
+  registerForm: FormGroup = new FormGroup({});
+
+  ngOnInit(): void {
+      this.registerForm = this.fb.group({
+        nombre: ['', Validators.required],
+        apellidos: ['', Validators.required],
+        correo: ['', Validators.required],
+        contraseña: ['', Validators.required],
+        confirmarContraseña: ['', Validators.required]
+      })
+  }
+
+  registro() {
+    this.registerForm.markAllAsTouched();
+    console.log(this.registerForm.get('nombre')?.value)
+    if ( this.registerForm.valid){
+      console.log('registrado')
+
+    }
+  }
+
+  validarCampo(campo: string) {
+    return this.registerForm.get(campo)?.invalid && (this.registerForm.get(campo)?.dirty || this.registerForm.get(campo)?.touched);
+  }
 }
--- src/app/pages/register/register.module.ts
+++ src/app/pages/register/register.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { RegisterRoutingModule } from './register-routing.module';
 import { RegisterComponent } from './register.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { SharedModule } from 'src/app/shared/shared.module';
   imports: [
     CommonModule,
     RegisterRoutingModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ],
   exports: [
     RegisterComponent
